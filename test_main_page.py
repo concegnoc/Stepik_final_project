db2752c609dc853e7f91aade6c7bce77e7d91565
concegnoc <ppk-00@mail.ru>
2020-10-26 20:16:31 +0300

    add changing files, that were added earlier

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,14 @@
-import time
+from pages.main_page import MainPage
 
-def test_should_check_bottom_of_adding_product_with_certain_language(browser):
-    button = browser.find_element_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
-    assert button
 
-    time.sleep(10)
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
